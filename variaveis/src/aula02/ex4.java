6200167060f562ac080f408b7e2e5aaa8dade8f1
balbino-z <oviniciusbalbino@gmail.com>
2025-04-14 16:19:25 -0300

    Realizacao dos exercicios propostos

--- variaveis/src/aula02/ex4.java
+++ variaveis/src/aula02/ex4.java
@@ -11,16 +11,16 @@ Scanner leia = new Scanner(System.in);
 		System.out.println("---Calculadora de diferencial---");
 		
 		
-		System.out.println("\n\nDigite 4 números: ");
+		System.out.println("\n\nDigite o 1 número: ");
 		float num1 = leia.nextFloat();
 
-		System.out.println("\n\nDigite 4 números: ");
+		System.out.println("\n\nDigite o 2 número: ");
 		float num2 = leia.nextFloat();
 		
-		System.out.println("\n\nDigite 4 números: ");
+		System.out.println("\n\nDigite o 3 número: ");
 		float num3 = leia.nextFloat();
 		
-		System.out.println("\n\nDigite 4 números: ");
+		System.out.println("\n\nDigite o 4 número: ");
 		float  num4 = leia.nextFloat();	
 		
 		System.out.printf("(%.2f * %.2f) - (%.2f * %.2f) O diferencial é = %.2f", num1, num2, num3, num4, (num1 * num2) - (num3 * num4));
